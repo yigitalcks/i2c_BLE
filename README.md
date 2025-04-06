380e5b4522b8a12206710559fa51de65e7491bf4
yigitalcks <yigitalkcs0@outlook.com>
2025-04-06 13:31:47 +0300

    final

---
+++ README.md
@@ -0,0 +1,5 @@
+## I2C
+
+
+
+![Scheme](borda.drawio.png)
--- esp32_master/main/i2c_eeprom_main.c
+++ esp32_master/main/i2c_eeprom_main.c
@@ -7,32 +7,161 @@
 #include "driver/i2c_master.h"
 #include "driver/i2c_slave.h"
 
+
 #define SCL_IO_PIN CONFIG_I2C_MASTER_SCL
 #define SDA_IO_PIN CONFIG_I2C_MASTER_SDA
 #define MASTER_FREQUENCY 100000
-#define SLAVE_ADRESS 0x55
+#define SLAVE_ADRESS_IMU 0x68
+#define SLAVE_ADRESS_TEMPERATURE 0x55 // TODO
 
-#define STACK_SIZE 128
-
-typedef struct {
-    i2c_master_dev_handle_t temp_sensor;
-    i2c_master_dev_handle_t imu_sensor;
-    QueueHandle_t rawDataQueue;
-} I2c_sensor_context;
+#define WINDOW_SIZE 3
 
 typedef struct {
-    QueueHandle_t filteredDataQueue;
-} I2c_filter_context;
+    int16_t acc_x;
+    int16_t acc_y;
+    int16_t acc_z;
+    
+    //int16_t gyro_x;
+    //int16_t gyro_y;
+    //int16_t gyro_z;
+} Mpu6050;
 
 typedef struct {
-    uint16_t temp;
-    uint32_t imu;
+    uint16_t temperature;
+    Mpu6050 imu;
 } Borda_package;
 
-// Producer
-void get_sensor_data();
-// Consumer
-void filter_data();
+// Veri okuma taskı için context
+typedef struct {
+    i2c_master_dev_handle_t dev_handle_temperature;
+    i2c_master_dev_handle_t dev_handle_imu;
+    QueueHandle_t rawDataQueue;
+} SensorConxtext;
+
+// Filtre taskı için context
+typedef struct {
+    QueueHandle_t filteredDataQueue;
+    QueueHandle_t rawDataQueue;
+} FilterContext;
+
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void debug_i2c(Borda_package borda) {
+    printf("Temperature: %hu\n", borda.temperature);
+    printf("IMU: \n\tacc_x = %u\nacc_y = %u\nacc_z = %u", borda.imu.acc_x, borda.imu.acc_y, borda.imu.acc_z);
+}
+
+
+//////////////////////////////////////////////////
+// I2C Slave cihazları başlatma ve okuma işlemleri
+//////////////////////////////////////////////////
+
+/**
+ * @brief MPU6050 sensörünü başlatır (uyandırır).
+ */
+void mpu_init(SensorConxtext* ctx) {
+    uint8_t reg1 = 0x6B;
+    uint8_t reg2 = 0x00;
+    ESP_ERROR_CHECK(i2c_master_transmit(ctx->dev_handle_imu, &reg1, 1, 1000));
+    ESP_ERROR_CHECK(i2c_master_transmit(ctx->dev_handle_imu, &reg2, 1, 1000));
+}
+
+/**
+ * @brief I2C slavelerden(Arduino ve Mpu6050) veri okuyup Queue'ya gönderir.
+ */
+void read_sensor_data(void* pvParameters) {
+    SensorConxtext* s_context = (SensorConxtext*) pvParameters;
+    
+    uint8_t imu_reg = 0x3B;
+
+    while(1) {
+        uint8_t temperature_buffer[2] = {0};
+        uint8_t imu_buffer[6] = {0}; //acc_x, acc_y and acc_z = 6 bytes
+
+        // Sıcaklık okuma (arduino)
+        ESP_ERROR_CHECK(i2c_master_receive(s_context->dev_handle_temperature, temperature_buffer, sizeof(temperature_buffer), 1000));
+        uint16_t temperature = temperature_buffer[0] << 8 | temperature_buffer[1]; 
+
+        // İvmeölçer okuma (MPU6050, acc_x, acc_y, acc_z)
+        ESP_ERROR_CHECK(i2c_master_transmit(s_context->dev_handle_imu, &imu_reg, 1, 1000));
+        ESP_ERROR_CHECK(i2c_master_receive(s_context->dev_handle_imu, imu_buffer, sizeof(imu_buffer), 1000));
+
+        uint16_t acc_x = (imu_buffer[0] << 8) | imu_buffer[1];
+        uint16_t acc_y = (imu_buffer[2] << 8) | imu_buffer[3];
+        uint16_t acc_z = (imu_buffer[4] << 8) | imu_buffer[5];
+
+        // Verilerin struct'a yerleştirilmesi
+        Borda_package borda = {
+            .temperature = temperature,
+            .imu = {
+                .acc_x = acc_x,
+                .acc_y = acc_y,
+                .acc_z = acc_z
+            }
+        };
+
+        xQueueSend(s_context->rawDataQueue, &borda, portMAX_DELAY);
+
+        // Periyot
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+//////////////////////////////////////////////////
+// Filtre işlemi
+//////////////////////////////////////////////////
+
+/**
+ * @brief Bir pencere içerisindeki sıcaklık değerinin medianını hesaplar.
+ */
+int temperature_median(uint16_t window[3]) {
+    uint16_t temperature[] = {window[0], window[1], window[2]};
+    qsort(temperature, sizeof(temperature) / sizeof(uint16_t), sizeof(uint16_t), compare);
+    return temperature[1];
+}
+
+/**
+ * @brief Raw data kuyruğundan(rawDataQueue) gelen verileri alır, pencere üzerinden filtre uygular
+ *        ve filtrelenmiş veri kuyruğuna(filteredDataQueue) gönderir.
+ */
+void filter_data(void* pvParameters) {
+
+    FilterContext* f_context = (FilterContext*) pvParameters;
+
+    Borda_package window[WINDOW_SIZE] = {0};
+    Borda_package rawData = {0};
+    Borda_package filteredData = {0};
+    uint32_t count = 0;
+    uint32_t idx = 1;
+    
+
+    while (1) {
+        if(xQueueReceive(f_context->rawDataQueue, &rawData, portMAX_DELAY) == pdTRUE) {
+
+            if(count < 1) {
+                window[idx] = rawData;
+                count++;
+                idx++;
+                continue;
+            }
+            window[idx] = rawData;
+            count++;
+            idx = (idx + 1) % 3;
+
+            filteredData.imu = rawData.imu;
+            uint16_t temperature_window[3] = {window[0].temperature, window[1].temperature, window[2].temperature};
+            filteredData.temperature = temperature_median(temperature_window);
+
+            xQueueSend(f_context->filteredDataQueue, &filteredData, portMAX_DELAY);
+            debug_i2c(filteredData);
+        }
+    }
+
+}
+
 
 void app_main(void)
 {
@@ -48,62 +177,48 @@ void app_main(void)
     i2c_master_bus_handle_t bus_handle;
     ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_config, &bus_handle));
 
-    i2c_device_config_t dev_cfg_temp = {
+    // I2c slave arduino
+    i2c_device_config_t dev_cfg_temperature = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-        .device_address = SLAVE_ADRESS,
+        .device_address = SLAVE_ADRESS_TEMPERATURE,
         .scl_speed_hz = MASTER_FREQUENCY
     };
 
-    i2c_master_dev_handle_t dev_handle_temp;
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg_temp, &dev_handle_temp));
+    static i2c_master_dev_handle_t dev_handle_temperature;
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg_temperature, &dev_handle_temperature));
 
+    //i2c slave MPU6050
     i2c_device_config_t dev_cfg_imu = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-        .device_address = SLAVE_ADRESS, // TODO
+        .device_address = SLAVE_ADRESS_IMU,
         .scl_speed_hz = MASTER_FREQUENCY
     };
 
-    i2c_master_dev_handle_t dev_handle_imu;
+    static i2c_master_dev_handle_t dev_handle_imu;
     ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg_imu, &dev_handle_imu));
     /////////////////////////////////////////////////////////////////////////////// 
+    static QueueHandle_t rawDataQueue;
+    static QueueHandle_t filteredDataQueue;
 
-    static I2c_sensor_context s_context = {0};
-    s_context.temp_sensor = dev_handle_temp;
-    s_context.imu_sensor = dev_handle_imu;
-    s_context.rawDataQueue = xQueueCreate(10, sizeof(Borda_package));
+    rawDataQueue = xQueueCreate(10, sizeof(Borda_package));
+    filteredDataQueue = xQueueCreate(10, sizeof(Borda_package));
 
-    static I2c_filter_context f_context = {0};
-    f_context.filteredDataQueue = xQueueCreate(10, sizeof(Borda_package));
+    static SensorConxtext s_context = {0};
+    s_context.dev_handle_temperature = dev_handle_temperature;
+    s_context.dev_handle_imu = dev_handle_imu;
+    s_context.rawDataQueue = rawDataQueue;
 
-    
-    xTaskCreate(get_sensor_data, "get_sensor_data", 1024, (void*) &s_context, tskIDLE_PRIORITY, NULL);
-    xTaskCreate(filter_data, "filter_data", 1024, (void*) &f_context, tskIDLE_PRIORITY, NULL);
+    static FilterContext f_context = {0};
+    f_context.filteredDataQueue = filteredDataQueue;
+    f_context.rawDataQueue = rawDataQueue;
+
+    mpu_init(&s_context);
+   
+
+    xTaskCreate(read_sensor_data, "get_sensor_data", 4096, (void*) &s_context, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(filter_data, "filter_data", 4096, (void*) &f_context, tskIDLE_PRIORITY, NULL);
+    // TODO Bluetooth broadcast taskı 
 }   
 
-void get_sensor_data(void* param) {
-    I2c_sensor_context* s_context = (I2c_sensor_context*) param;
-    
-    while(1) {
-        uint8_t temp_buffer[2] = {0};
-        uint8_t imu_buffer[3] = {0};
 
-        ESP_ERROR_CHECK(i2c_master_receive(s_context->temp_sensor, temp_buffer, sizeof(temp_buffer), 1000));
-        uint16_t temp = temp_buffer[0] << 8 | temp_buffer[1]; 
-
-        ESP_ERROR_CHECK(i2c_master_receive(s_context->imu_sensor, imu_buffer, sizeof(imu_buffer), 1000));
-        uint32_t imu = 0; // TODO
-
-        Borda_package borda = {
-            .temp = temp,
-            .imu = imu
-        };
-
-        xQueueSend(s_context->rawDataQueue, &borda, portMAX_DELAY);
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-void filter_data(void* param) {
-
-}
+// TODO BLUETOOTH taskı yap
